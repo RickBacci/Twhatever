21c8cc5c8c0f19443142ab671d99a5f71de75390
Rick Bacci <rickbacci@gmail.com>
2015-09-08 15:16:57 -0600

    change syntax of spec

--- spec/features/authenticate_with_twitter_spec.rb
+++ spec/features/authenticate_with_twitter_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "User", :type => :feature do
+feature "User" do
+  scenario "can authenticate with Twitter" do
 
-  it "can authenticate with Twitter" do
     visit root_path
 
     click_on "Login with Twitter"
