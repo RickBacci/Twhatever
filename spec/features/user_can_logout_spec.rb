3d9a373150985dde1ac55890d246cb7a662d0fc2
Rick Bacci <rickbacci@gmail.com>
2015-09-08 21:57:02 -0600

    add user can logout spec

---
+++ spec/features/user_can_logout_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature "User" do
+  include OmniAuthUser
+
+  before do
+    OmniAuth.config.mock_auth[:twitter] = nil
+    stub_omniauth
+  end
+
+  scenario "can logout of Twitter" do
+
+    visit root_path
+
+    expect(page).to_not have_link('Logout')
+
+    click_on "Login"
+
+    expect(page).to have_link('Logout')
+
+    click_link "Logout"
+
+    expect(page).to have_link('Login')
+  end
+end
