d82d957e10227db19cd3634cdcd15ee6bf0084ff
Rick Bacci <rickbacci@gmail.com>
2015-09-09 01:18:59 -0600

    add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
+
   root 'home#index'
 
   get '/auth/twitter', as: '/login'
   get '/auth/twitter/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  get '/profile', to: 'user#show'
+  get '/users/:id', as: :user, to: 'users#show'
 end
