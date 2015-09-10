ccc81bb1b9e6acab61ed856dcec0f5194d824285
Rick Bacci <rickbacci@gmail.com>
2015-09-09 23:53:08 -0600

    add see profile info test

---
+++ spec/features/user_can_see_profile_info_on_homepage_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature "User" do
+  include OmniAuthUser
+
+  before do
+    OmniAuth.config.mock_auth[:twitter] = nil
+    OmniAuth.config.test_mode = true
+    stub_omniauth
+  end
+
+  scenario "can see profile info on home page" do
+    VCR.use_cassette("home-profile") do
+      visit root_path
+      click_on "Login"
+
+      expect(page).to have_content('Rick Bacci')
+      expect(page).to have_content('@rickbacci_175')
+      expect(page).to have_content('tweets')
+      expect(page).to have_content('following')
+      expect(page).to have_content('followers')
+    end
+  end
+end
