a54bfb52858ebdf9022b5d7ab3f34be08496ee64
Rick Bacci <rickbacci@gmail.com>
2015-09-10 18:27:27 -0600

    fix favorite action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,13 @@ class UsersController < ApplicationController
   def show
   end
 
-
   def tweet
     current_user.twitter_client.update(params[:tweet])
     redirect_to root_path
   end
 
   def favorite
-    # current_user.twitter_client.favorite(params[:favorite])
-    favorite_tweet(params[:tweet])
+    current_user.twitter_client.favorite(params[:favorite])
     redirect_to root_path
   end
 end
